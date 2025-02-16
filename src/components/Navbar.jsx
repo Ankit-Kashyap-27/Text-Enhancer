bbfa6076f0cd2619eb7d6918cac7a321682384e8
Ankit-Kashyap-27 <ankitkashyap73400@gmail.com>
2025-02-16 19:36:25 +0530

    Basic Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-green-50 py-3 '>
+            <div className='flex justify-between items-center '>
+                <div className='flex gap-7 '>
+                    <div className=" mx-2">
+                        <span className='text-xl'>TextUtils</span>
+                    </div>
+
+                    <div>
+                        <ul>
+                            <li className='text-lg font-thin'>
+                                <a className='mx-2' href="/">Home</a>
+                                <a className='mx-2' href="/">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='flex gap-4 items-center '>
+                    <input className='text-lg  p-2 rounded-sm border-2 border-slate-300' placeholder='   Search'   type="text" />
+                    <button className=' mx-3 px-4 py-2 rounded-md  border-2 border-green-700 '>Search</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
