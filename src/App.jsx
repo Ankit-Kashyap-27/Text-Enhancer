89d5e4fecc065f11fd3c96b78737a8ed56ead69a
Ankit-Kashyap-27 <ankitkashyap73400@gmail.com>
2025-02-22 17:41:37 +0530

    darkmode slider done but right now it is not functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import { useContext } from 'react'
 
 
 function App() {
@@ -10,29 +11,56 @@ function App() {
   }
 
   const handleUpClick = (event) => {
-let newtext= Text.toUpperCase();
-setText(newtext)
+    let newtext = Text.toUpperCase();
+    setText(newtext)
+  }
+  const handlelowClick = (event) => {
+    
+    let newtext = Text.toLowerCase();
+    setText(newtext)
+  }
+  const handeClear = () => {
+    setText("")
   }
 
   return (
     <>
+
       <div className="">
         <Navbar title="TestUtils" />
       </div>
-      <div className=" flex  flex-col items-center  h-[100vh] ">
 
-        <div className=" mt-[10vh] ">
+      <div className=" container flex  flex-col items-center  ">
+
+        <div className=" mt-[8vh] ">
           <div className=" text-3xl font-semibold m-3">
             <h1>Enter The TEXT to analyze below</h1>
           </div>
 
           <div className=" ">
-            <textarea className='w-[70vw] border-4 border-blue-300 rounded-lg text-xl ' onChange={handleOnchange} name="" id="" value={Text} rows={8}></textarea>
+            <textarea className='w-[70vw] border-4 border-blue-300 rounded-lg text-xl ' onChange={handleOnchange} name="" id="" value={Text} rows={7}></textarea>
           </div>
 
           <div className="">
-            <button className='bg-blue-500 p-3 text-xl font-semibold rounded-md text-white ' onClick={handleUpClick}>Upper Case</button>
+            <button className='bg-blue-500 mx-2 p-3 text-xl font-semibold rounded-md text-white ' onClick={handleUpClick}>Upper Case</button>
+            <button className='bg-blue-500 mx-2 p-3 text-xl font-semibold rounded-md text-white ' onClick={handlelowClick}>Lower Case</button>
+            <button className='bg-blue-500 mx-2 p-3 text-xl font-semibold rounded-md text-white ' onClick={handeClear}>CLEAR</button>
           </div>
+            
+        <div className="  w-full m-5  ">
+        
+            <h1 className='text-4xl my-3  font-semibold'>Your Text Summery </h1>
+
+            <p className='text-xl my-2'>{Text.split(" ").length} Words and {Text.length} Characters</p>
+
+            <p className='text-xl'>Its going to take {0.008 * Text.split(" ").length} Minutes To read</p>
+
+            <h2 className='text-3xl my-2'>Preview</h2>
+
+            <p className='text-xl my-2'> {Text}</p>
+          
+        </div>
+
 
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import sun from "../assets/sun.svg"
+import moon from "../assets/moon.svg"
 
 
 const Navbar = (props) => {
-    return (
-        <nav className='bg-green-50 py-3 '>
-            <div className='flex justify-between items-center '>
-                <div className='flex gap-7 '>
-                    <div className=" mx-2">
-                        <span className='text-xl'>{props.title}</span>
-                    </div>
 
-                    <div>
-                        <ul>
-                            <li className='text-lg font-thin'>
-                                <a className='mx-2' href="/">Home</a>
-                                <a className='mx-2' href="/">About</a>
-                            </li>
-                        </ul>
+    const [isDarkmode, setisDarkmode] = useState(false);
+
+    const handleSlide = () => {
+        setisDarkmode(!isDarkmode);
+        document.body.classList.toggle('Darkmode', !isDarkmode)
+    }
+    return (
+        <nav className='bg-green-100 py-2 h-[10vh] flex items-center  justify-between px-8'>
+            <div className=' gap-7  flex  '>
+                <span className='text-2xl'>{props.title}</span>
+                <ul>
+                    <li className='text-lg  font-thin'>
+                        <a classNamre='mx-4 duration-200 hover:border-b-2 hover:boder-b-black' href="/">Home</a>
+
+                        <a className='mx-4  duration-200 hover:border-b-2 hover:border-b-black' href="/">About</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="">
+                <div className='bg-[#36454F] rounded-full h-[6vh] w-[9vw] flex items-center'>
+                    <div onClick={handleSlide} className={ `transition-transform duration-500 bg-[#e4e4e3] h-[6vh] w-[3vw] rounded-full flex justify-center items-center ${!isDarkmode ? 'translate-x-[6vw]' : 'translate-x-0'}`}>
+                        <img className='size-6' src={isDarkmode ? sun : moon} alt="" />
                     </div>
-                </div>
-                <div className='flex gap-4 items-center '>
-                    <input className='text-lg  p-2 rounded-sm border-2 border-slate-300' placeholder='   Search'   type="text" />
-                    <button className=' mx-3 px-4 py-2 rounded-md  border-2 border-green-700 '>Search</button>
                 </div>
             </div>
+
         </nav>
     )
 }
 
 export default Navbar
+
+
+
+
+
 
